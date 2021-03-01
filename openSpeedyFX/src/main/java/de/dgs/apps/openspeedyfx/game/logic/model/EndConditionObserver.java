f3af82f4ba2975712f1e2001838be8212f9eb8f0
Leander Steiner <leandersteiner@yahoo.de>
2021-03-01 15:54:35 +0100

    EndConditionObserver now correctly removes itself once a player has won or lost and observing them is no loner necessary.

--- openSpeedyFX/src/main/java/de/dgs/apps/openspeedyfx/game/logic/model/EndConditionObserver.java
+++ openSpeedyFX/src/main/java/de/dgs/apps/openspeedyfx/game/logic/model/EndConditionObserver.java
@@ -1,6 +1,7 @@
 package de.dgs.apps.openspeedyfx.game.logic.model;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class EndConditionObserver implements Observer<Actor>{
@@ -17,15 +18,17 @@ public class EndConditionObserver implements Observer<Actor>{
         if(updateActor instanceof Player){
             if(updateActor.getCurrentTile().getTileType() == TileType.END){
                 gameMode.playerWon((Player) updateActor);
+                updateActor.unregister(this);
                 gameMode.endTurn();
             }
         }
 
         if(updateActor instanceof NPC){
-            List<Player> players = gameMode.getPlayers();
+            List<Player> players = new ArrayList<>(gameMode.getPlayers());
             for(Player p : players){
                 if(p.getCurrentTile() == updateActor.getCurrentTile()){
                     gameMode.playerLost(p);
+                    p.unregister(this);
                 }
                 p.getCurrentTile().getAdjacent().forEach(tile -> {
                     if(tile == updateActor.getCurrentTile()){
